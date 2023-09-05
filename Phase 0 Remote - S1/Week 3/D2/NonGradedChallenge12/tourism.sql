6488761bc1cf39763de73f396c192042d75cae98
ALFIKRI <alfikri.name@gmail.com>
2023-09-05 17:14:29 +0700

    add guest

--- Phase 0 Remote - S1/Week 3/D2/NonGradedChallenge12/tourism.sql
+++ Phase 0 Remote - S1/Week 3/D2/NonGradedChallenge12/tourism.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 05/09/2023 16:39:14
+ Date: 05/09/2023 17:14:09
 */
 
 SET NAMES utf8mb4;
@@ -23,22 +23,24 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `bookings`;
 CREATE TABLE `bookings`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guest_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `guest_id` int(11) NOT NULL,
   `check_in_date` date NULL DEFAULT NULL,
   `check_out_date` date NULL DEFAULT NULL,
   `hotel_id` int(11) NULL DEFAULT NULL,
   `total_price` decimal(10, 2) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `hotel_id`(`hotel_id`) USING BTREE,
-  CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `guest_id`(`guest_id`) USING BTREE,
+  CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `guests` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of bookings
 -- ----------------------------
-INSERT INTO `bookings` VALUES (1, 'John Doe', '2023-09-10', '2023-09-15', 1, 1500.00);
-INSERT INTO `bookings` VALUES (2, 'Alice Smith', '2023-10-05', '2023-10-10', 2, 1800.00);
-INSERT INTO `bookings` VALUES (3, 'Bob Johnson', '2023-11-20', '2023-11-25', 3, 1200.00);
+INSERT INTO `bookings` VALUES (1, 1, '2023-09-10', '2023-09-15', 1, 1500.00);
+INSERT INTO `bookings` VALUES (2, 1, '2023-10-05', '2023-10-10', 2, 1800.00);
+INSERT INTO `bookings` VALUES (3, 1, '2023-11-20', '2023-11-25', 3, 1200.00);
 
 -- ----------------------------
 -- Table structure for destinations
@@ -59,6 +61,22 @@ CREATE TABLE `destinations`  (
 INSERT INTO `destinations` VALUES (1, 'Bali', 'Pulau indah di Indonesia', 'Indonesia', 4.50);
 INSERT INTO `destinations` VALUES (2, 'Paris', 'Kota cinta di Prancis', 'Prancis', 4.70);
 INSERT INTO `destinations` VALUES (3, 'New York', 'Kota yang tidak pernah tidur di Amerika Serikat', 'Amerika Serikat', 4.30);
+
+-- ----------------------------
+-- Table structure for guests
+-- ----------------------------
+DROP TABLE IF EXISTS `guests`;
+CREATE TABLE `guests`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of guests
+-- ----------------------------
+INSERT INTO `guests` VALUES (1, 'AL', 'FIKRI');
 
 -- ----------------------------
 -- Table structure for hotels
